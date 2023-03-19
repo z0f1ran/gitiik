740ad5770b45290e5af60c41d1b265bac1ed569a
top_acdemy_student <top_acdemy_student@OR-08>
2023-03-19 12:39:39 +0700

    новая фича

--- gitiik/Program.cs
+++ gitiik/Program.cs
@@ -8,8 +8,14 @@ namespace gitiik
 {
     internal class Program
     {
+        static void NewFeature()
+        {
+            Console.WriteLine("this is new Feature");
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("v2");
+            NewFeature();
         }
     }
 }
